2ad288e613e8a35ff90f50e8e27ffa63dcf53193
Ingmar Delsink <ingmardelsink@hotmail.com>
2025-07-27 18:51:13 +0200

    Enable bash completion when not enabled yet

--- home/exact_dot_bashrc.d/executable_03-bash-config.sh
+++ home/exact_dot_bashrc.d/executable_03-bash-config.sh
@@ -16,3 +16,12 @@ HISTCONTROL="ignorespace:erasedups"
 # '-'  - Separator for better readability.
 # More details: https://www.gnu.org/software/bash/manual/bash.html#index-HISTTIMEFORMAT
 HISTTIMEFORMAT='%F %T - '
+
+# Enable bash completion in interactive shells
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    source /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    source /etc/bash_completion
+  fi
+fi
