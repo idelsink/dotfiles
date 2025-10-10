a83d16c9d551eb3cd89fadbda00e2fb4020e4ba8
Ingmar Delsink <ingmardelsink@hotmail.com>
2025-10-10 23:02:21 +0200

    feat: Update mise completion

--- home/exact_dot_bashrc.d/executable_05-mise.sh
+++ home/exact_dot_bashrc.d/executable_05-mise.sh
@@ -3,5 +3,9 @@
 # Configure Mise
 
 if [[ -x "$(command -v mise)" ]]; then
+  # https://mise.jdx.dev/installing-mise.html#autocompletion
+  # This requires bash-completion to be installed
+  mkdir -p "${HOME}/.local/share/bash-completion/completions/"
+  mise completion bash --include-bash-completion-lib > "${HOME}/.local/share/bash-completion/completions/mise"
   eval "$(mise activate bash)"
 fi
