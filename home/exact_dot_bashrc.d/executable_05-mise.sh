8b9a2d03a1c5fbd0a558005e6d83c09194517612
Ingmar Delsink <ingmardelsink@hotmail.com>
2025-11-06 14:14:54 +0100

    feat: Add mise usage if not installed

--- home/exact_dot_bashrc.d/executable_05-mise.sh
+++ home/exact_dot_bashrc.d/executable_05-mise.sh
@@ -2,9 +2,14 @@
 #
 # Configure Mise
 
-if [[ -x "$(command -v mise)" ]]; then
+if which mise >/dev/null; then
   # https://mise.jdx.dev/installing-mise.html#autocompletion
   # This requires bash-completion to be installed
+  #
+  if ! which usage >/dev/null; then
+    echo "Usage (for mise) is not installed, installing it now..."
+    mise use --global usage
+  fi
   mkdir -p "${HOME}/.local/share/bash-completion/completions/"
   mise completion bash --include-bash-completion-lib > "${HOME}/.local/share/bash-completion/completions/mise"
   eval "$(mise activate bash)"
