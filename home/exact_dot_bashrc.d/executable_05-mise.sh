ead170282b36646dd9cd32a8bda3f5d6a18f6d76
Ingmar Delsink <ingmardelsink@hotmail.com>
2025-11-06 14:16:44 +0100

    feat: Activate mise before completion

--- home/exact_dot_bashrc.d/executable_05-mise.sh
+++ home/exact_dot_bashrc.d/executable_05-mise.sh
@@ -6,11 +6,11 @@ if which mise >/dev/null; then
   # https://mise.jdx.dev/installing-mise.html#autocompletion
   # This requires bash-completion to be installed
   #
+  eval "$(mise activate bash)"
   if ! which usage >/dev/null; then
     echo "Usage (for mise) is not installed, installing it now..."
     mise use --global usage
   fi
   mkdir -p "${HOME}/.local/share/bash-completion/completions/"
   mise completion bash --include-bash-completion-lib > "${HOME}/.local/share/bash-completion/completions/mise"
-  eval "$(mise activate bash)"
 fi
